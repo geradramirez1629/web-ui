356ee5e50d82ecfe47f4095769092d3903d18cb5
kevin odwyer <kevodwyer@gmail.com>
2022-04-26 16:59:41 +0100

    redo shortcuts

--- src/store/index.js
+++ src/store/index.js
@@ -55,6 +55,9 @@ module.exports = new Vuex.Store({
             appMimeTypeRegistrationMap: new Map(),
             appFileTypeRegistrationMap: new Map(),
             appsInstalled: []
+        },
+        shortcuts: {
+            shortcutsMap: new Map()
         }
 	},
 
@@ -230,7 +233,11 @@ module.exports = new Vuex.Store({
         },
         SET_SANDBOXED_APPS(state, payload) {
             state.sandboxedApps.appsInstalled = payload;
-        }
+        },
+        //shortcuts
+        SET_SHORTCUTS(state, payload) {
+            state.shortcuts.shortcutsMap = payload;
+        },
 	},
 
 	// Async
